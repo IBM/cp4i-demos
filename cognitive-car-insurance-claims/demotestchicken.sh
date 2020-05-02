50c91e38184f6f5d836aade583f9eb4628065a3c
James Hewitt <james.hewitt@uk.ibm.com>
2020-05-02 19:32:55 +0100

    Make scripts neat for demos

--- cognitive-car-insurance-claims/demotestchicken.sh
+++ cognitive-car-insurance-claims/demotestchicken.sh
@@ -1,1 +1,17 @@
-curl -k -u $cp4iuser:$cp4ipw --request POST   --url $cp4ibasepath/CarRepairClaim --header "X-IBM-Client-Id:$cp4iclientid"  --header 'accept: application/json'   --header 'content-type: application/json'   --data '{"Name":"Vernon Barker","eMail":"to@epiope.my","LicensePlate":"tepuru","DescriptionOfDamage":"58","PhotoOfCar":"'$(base64 chicken.jpg)'","ContactID":"8897796795006976"}'
+#!/bin/sh
+
+curl --insecure \
+     --user "$cp4iuser:$cp4ipw" \
+     --request POST \
+     --url "$cp4ibasepath/CarRepairClaim" \
+     --header "X-IBM-Client-Id:$cp4iclientid" \
+     --header 'accept: application/json' \
+     --header 'content-type: application/json' \
+     --data '{
+         "Name":"Vernon Barker",
+         "eMail":"to@epiope.my",
+         "LicensePlate":"tepuru",
+         "DescriptionOfDamage":"58",
+         "PhotoOfCar":"'$(base64 chicken.jpg)'",
+         "ContactID":"8897796795006976"
+         }'
--- cognitive-car-insurance-claims/demotestroadster.sh
+++ cognitive-car-insurance-claims/demotestroadster.sh
@@ -1,1 +1,17 @@
-curl -k -u $cp4iuser:$cp4ipw --request POST   --url $cp4ibasepath/CarRepairClaim --header "X-IBM-Client-Id:$cp4iclientid"  --header 'accept: application/json'   --header 'content-type: application/json'   --data '{"Name":"Brenda Beemer","eMail":"beemerbrenda@example.com","LicensePlate":"ILUVZ4","DescriptionOfDamage":"Turbo is down on power and I like to drive fast - please tune it up!","PhotoOfCar":"'$(base64 roadster.jpg)'","ContactID":"8897796795006976"}'
+#!/bin/sh
+
+curl --insecure \
+     --user "$cp4iuser:$cp4ipw" \
+     --request POST \
+     --url "$cp4ibasepath/CarRepairClaim" \
+     --header "X-IBM-Client-Id:$cp4iclientid" \
+     --header 'accept: application/json' \
+     --header 'content-type: application/json' \
+     --data '{
+         "Name":"Brenda Beemer",
+         "eMail":"beemerbrenda@example.com",
+         "LicensePlate":"ILUVZ4",
+         "DescriptionOfDamage":"Turbo is down on power and I like to drive fast - please tune it up!",
+         "PhotoOfCar":"'$(base64 roadster.jpg)'",
+         "ContactID":"8897796795006976"
+         }'
